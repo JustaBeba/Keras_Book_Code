abc79a7179431ea267cb762c157f0b689cc5a91b
JustaBeba <72780321+JustaBeba@users.noreply.github.com>
2020-10-16 16:20:57 +0800

    Update 2-5.py

--- 2-5.py
+++ 2-5.py
@@ -23,7 +23,7 @@ network.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['ac
 train_images = train_images.reshape((60000,28 * 28))
 train_images = train_images.astype('float32')/255
 
-test_images = test_images.reshape((60000,28 * 28))
+test_images = test_images.reshape((10000,28 * 28))
 test_images = test_images.astype('float32')/255
 #label asset
 train_labels = to_categorical(train_labels)
